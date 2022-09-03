73badea602912ab2776abf525cb97cf9905ddcec
vavryshchuk <112618316+vavryshchuk@users.noreply.github.com>
2022-09-03 16:44:22 +0300

    Update IntRepresentation.java

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -12,10 +12,10 @@ public class IntRepresentation {
            int valueInt4= 114;
        char valueChar4= (char) valueInt4;
        
-       System.out.print("'"+valueChar1+"' ,");
-       System.out.print("'"+valueChar2+"' ,");
-       System.out.print("'"+valueChar3+"' ,");
-       System.out.print("'"+valueChar4+"' ");
+       System.out.print("'"+valueChar1+"', ");
+       System.out.print("'"+valueChar2+"', ");
+       System.out.print("'"+valueChar3+"', ");
+       System.out.print("'"+valueChar4+"'");
         
         }
     }
